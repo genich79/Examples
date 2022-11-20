3ec817b0e545a18725d6294f122ac4e8f2a8c15c
Johnny <genich79@gmail.com>
2022-11-20 14:28:15 +0300

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -2,3 +2,10 @@
 int numberB = 4;
 int result = numberA + numberB;
 Console.WriteLine(result);
+
+int numberC = new Random().Next(1, 10);
+Console.WriteLine(numberC);
+int numberD = new Random().Next(1, 10);
+Console.WriteLine(numberD);
+int result1 = numberC + numberD;
+Console.WriteLine(result);
