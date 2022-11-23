c765988c326c8aef60045aeff96aa3e767a53259
Johnny <genich79@gmail.com>
2022-11-23 20:13:17 +0300

    Example 010 IntroArray2

---
+++ Example010_IntroArray2/Program.cs
@@ -0,0 +1,27 @@
+﻿// int result = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+// Console.WriteLine(result);
+// Имеется одномерный массив array из n элементов, требуется
+// найти элемент массива, равный find
+// 1. Установить счетчик index в позицию 0
+// 2. Если array [index] = find, алгоритм завершил работу
+// успешно.
+// 3. Увеличить index на 1
+// 4. Если index < n, то перейти к шагу 2. В противном случае
+// алгоритм завершил работу безуспешно.
+
+int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+        if(array[index] == find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+        index++;
+}
